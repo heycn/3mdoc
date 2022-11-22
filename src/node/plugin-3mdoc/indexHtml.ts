49d6a55b38a762fa33193a63bfa8be2a1e9a5419
heycn <heycn@foxmail.com>
2022-11-22 15:50:57 +0800

    支持组件热更新

--- src/node/plugin-3mdoc/indexHtml.ts
+++ src/node/plugin-3mdoc/indexHtml.ts
@@ -1,18 +1,40 @@
-import { Plugin } from 'vite'
 import { readFile } from 'fs/promises'
-import { DEFAULT_TEMPLATE_PATH } from '../constants/index'
+import { Plugin } from 'vite'
+import { CLIENT_ENTRY_PATH, DEFAULT_HTML_PATH } from '../constants'
 
 export function pluginIndexHtml(): Plugin {
   return {
     name: '3mdoc:index-html',
+    apply: 'serve',
+    // 插入入口 script 标签
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: 'script',
+            attrs: {
+              type: 'module',
+              src: `/@fs/${CLIENT_ENTRY_PATH}`
+            },
+            injectTo: 'body'
+          }
+        ]
+      }
+    },
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
-          // 1. 读取 template.html 内容
-          const content = await readFile(DEFAULT_TEMPLATE_PATH, 'utf-8')
-          // 2. 响应 HTML 的内容给浏览器
-          res.setHeader('Content-Type', 'text/html')
-          res.end(content)
+          let html = await readFile(DEFAULT_HTML_PATH, 'utf-8')
+
+          try {
+            html = await server.transformIndexHtml(req.url, html, req.originalUrl)
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/html')
+            res.end(html)
+          } catch (error) {
+            return next(error)
+          }
         })
       }
     }
