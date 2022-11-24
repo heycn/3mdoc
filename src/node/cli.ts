6b3d6978cf52ef77d1c333f086a3c48f81c1b45a
heycn <heycn@foxmail.com>
2022-11-24 17:47:08 +0800

    debug: SSG 核心流程开发

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,4 +1,5 @@
 import cac from 'cac'
+import { build } from './build'
 import { createDevServer } from './dev'
 
 const cli = cac('3mdoc').version('0.0.1').help()
@@ -10,7 +11,7 @@ cli.command('dev [root]', 'start dev server').action(async (root: string) => {
 })
 
 cli.command('build [root]', 'build in production').action(async (root: string) => {
-  console.log('build', root)
+  await build(root)
 })
 
 cli.parse()
