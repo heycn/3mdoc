214eaf7be367bf38c063340f7f278a06cf881bd1
heycn <heycn@foxmail.com>
2023-05-31 01:16:44 +0800

    搭建 E2E 测试环境

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,11 +1,17 @@
 import { createServer } from 'vite'
 import { pluginIndexHtml } from './plugin-3mdoc/indexHtml'
 import pluginReact from '@vitejs/plugin-react'
+import { PACKAGE_ROOT } from './constants'
 
 export const createDevServer = (root: string) => createServer({
   root,
   plugins: [
     pluginIndexHtml(),
     pluginReact()
-  ]
+  ],
+  server: {
+    fs: {
+      allow: [PACKAGE_ROOT]
+    }
+  }
 })
