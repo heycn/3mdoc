13dcd2476d37c06cc60f46ed8fcd2efaeacd8ed4
heycn <heycn@foxmail.com>
2023-05-30 17:58:38 +0800

    可以使用 build 了

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,8 +1,13 @@
 import cac from "cac"
 
 const cli = cac("3mdoc").version("0.0.1").help()
-cli.command("dev [root]", "start dev server").action(async (root: string) => {
-  console.log("dev", root)
-})
+
+cli
+  .command("dev [root]", "start dev server")
+  .action(async (root: string) => { console.log("dev", root) })
+
+cli
+  .command("build [root]", "build in production")
+  .action(async (root: string) => { console.log("build", root) })
 
 cli.parse()
