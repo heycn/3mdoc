1162556f935f25d85adca0cf30c8fe02874a1a94
heycn <heycn@foxmail.com>
2023-05-31 10:22:00 +0800

    通用配置文件解析逻辑开发

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,16 +2,22 @@ import { createServer } from 'vite'
 import { pluginIndexHtml } from './plugin-3mdoc/indexHtml'
 import pluginReact from '@vitejs/plugin-react'
 import { PACKAGE_ROOT } from './constants'
+import { resolveConfig } from './config'
 
-export const createDevServer = (root: string) => createServer({
-  root,
-  plugins: [
-    pluginIndexHtml(),
-    pluginReact()
-  ],
-  server: {
-    fs: {
-      allow: [PACKAGE_ROOT]
+export async function createDevServer(root: string) {
+  const config = await resolveConfig(root, 'serve', 'development')
+  console.log(`[3mdoc] ${config}`)
+
+  return createServer({
+    root,
+    plugins: [
+      pluginIndexHtml(),
+      pluginReact()
+    ],
+    server: {
+      fs: {
+        allow: [PACKAGE_ROOT]
+      }
     }
-  }
-})
+  })
+}
