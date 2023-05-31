1ee9d0b1d95d21a6eb3f4f046e92cdbb2c6657b3
heycn <heycn@foxmail.com>
2023-05-31 22:58:26 +0800

    开发阶段 MDX 模块热更新实现

--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -5,3 +5,4 @@ export const RUNTIME_PATH = join(PACKAGE_ROOT, 'src', 'runtime')
 export const CLIENT_ENTRY_PATH = join(RUNTIME_PATH, 'client-entry.tsx')
 export const SERVER_ENTRY_PATH = join(RUNTIME_PATH, 'ssr-entry.tsx')
 export const DEFAULT_HTML_PATH = join(PACKAGE_ROOT, 'template.html')
+export const MD_REGEX = /\.mdx?$/
