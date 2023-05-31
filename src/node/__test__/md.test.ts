34480a08efdaf0b7e2d18254ce6e7c82be0f9862
heycn <heycn@foxmail.com>
2023-05-31 21:50:26 +0800

    实现 TOC 解析插件

--- src/node/__test__/md.test.ts
+++ src/node/__test__/md.test.ts
@@ -6,6 +6,9 @@ import rehypeStringify from 'rehype-stringify'
 import { rehypePluginPreWrapper } from '../plugin-mdx/rehypePlugins/preWrapper'
 import { rehypePluginShiki } from '../../node/plugin-mdx/rehypePlugins/shiki'
 import shiki from 'shiki'
+import remarkMdx from 'remark-mdx'
+import remarkStringify from 'remark-stringify'
+import { remarkPluginToc } from 'node/plugin-mdx/remarkPlugins/toc'
 
 describe('Markdown compile cases', async () => {
   const processor = unified()
@@ -26,8 +29,7 @@ describe('Markdown compile cases', async () => {
   test('Compile code', async () => {
     const mdContent = 'I am using `3mdoc.js`'
     const result = processor.processSync(mdContent)
-    expect(result.value).toMatchInlineSnapshot(
-    '"<p>I am using <code>3mdoc.js</code></p>"')
+    expect(result.value).toMatchInlineSnapshot('"<p>I am using <code>3mdoc.js</code></p>"')
   })
 
   test('Compile code block', async () => {
@@ -37,5 +39,17 @@ describe('Markdown compile cases', async () => {
       "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre class=\\"shiki monokai\\" style=\\"background-color: #272822\\" tabindex=\\"0\\"><code><span class=\\"line\\"><span style=\\"color: #F8F8F2\\">console.</span><span style=\\"color: #A6E22E\\">log</span><span style=\\"color: #F8F8F2\\">(</span><span style=\\"color: #AE81FF\\">123</span><span style=\\"color: #F8F8F2\\">);</span></span>
       <span class=\\"line\\"></span></code></pre></div>"
     `)
+  })
+
+  test('Compile TOC', async () => {
+    const remarkProcessor = unified()
+      .use(remarkParse)
+      .use(remarkMdx)
+      .use(remarkPluginToc)
+      .use(remarkStringify)
+    const mdContent = '## title `xxx` [link](/path)'
+    const result = remarkProcessor.processSync(mdContent)
+    expect(result.value.toString().replace(mdContent, ''))
+      .toMatchInlineSnapshot()
   })
 })
