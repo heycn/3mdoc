c70e7f75e2c77ddc46ff20da53ef1eaa29cd2e16
heycn <heycn@foxmail.com>
2023-06-01 11:45:36 +0800

    开发 Home 页面：开发基础组件

--- src/node/unocssOptions.ts
+++ src/node/unocssOptions.ts
@@ -3,6 +3,40 @@ import { presetAttributify, presetWind, presetIcons } from 'unocss'
 
 const options: VitePluginConfig = {
   presets: [presetAttributify(), presetWind({}), presetIcons()],
+  shortcuts: {
+    'flex-center': 'flex justify-center items-center'
+  },
+  theme: {
+    colors: {
+      brandLight: 'var(--3mdoc-c-brand-light)',
+      brandDark: 'var(--3mdoc-c-brand-dark)',
+      brand: 'var(--3mdoc-c-brand)',
+      text: {
+        1: 'var(--3mdoc-c-text-1)',
+        2: 'var(--3mdoc-c-text-2)',
+        3: 'var(--3mdoc-c-text-3)',
+        4: 'var(--3mdoc-c-text-4)'
+      },
+      divider: {
+        default: 'var(--3mdoc-c-divider)',
+        light: 'var(--3mdoc-c-divider-light)',
+        dark: 'var(--3mdoc-c-divider-dark)'
+      },
+      gray: {
+        light: {
+          1: 'var(--3mdoc-c-gray-light-1)',
+          2: 'var(--3mdoc-c-gray-light-2)',
+          3: 'var(--3mdoc-c-gray-light-3)',
+          4: 'var(--3mdoc-c-gray-light-4)'
+        }
+      },
+      bg: {
+        default: 'var(--3mdoc-c-bg)',
+        soft: 'var(--3mdoc-c-bg-soft)',
+        mute: 'var(--3mdoc-c-bg-mute)'
+      }
+    }
+  },
   rules: [
     [
       /^divider-(\w+)$/,
